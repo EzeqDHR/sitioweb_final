286fbc3dfd4fa4fb81f48ec0badb063a394c9f96
EzeqDHR <ezequeildaher06@gmail.com>
2022-08-27 12:03:19 -0300

    Se agraga el sitioweb

---
+++ codigo.js
@@ -0,0 +1,10 @@
+document.querySelector(`.menu-btn`).addEventListener(`click`,()=>{
+    document.querySelector(`.nav-menu`).classList.toggle(`show`)
+})
+
+ScrollReveal().reveal('.titulo');
+ScrollReveal().reveal('.texto',{delay:300});
+ScrollReveal().reveal('.container',{delay:300});
+ScrollReveal().reveal('.new-cards',{delay:300});
+ScrollReveal().reveal('.social',{delay:300});
+ScrollReveal().reveal('.subtitulo',{delay:300});
